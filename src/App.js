2671012db2ec8e6f0f274f143ff154f1c6bfffcc
dlvtt <noise_stormjk@mail.ru>
2020-11-28 17:04:14 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>hi</div>
-  );
+  return <Switch>
+
+    <Route exact path="/">
+      This is homepage
+    </Route>
+
+    <Route exact path="/starred">
+      This is starred
+    </Route>
+
+    <Route>
+      This is 404 page
+    </Route>
+    
+    </Switch>;
 }
 
 export default App;
